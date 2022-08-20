8ce30e1c244fad0b450c4df4ab6e51ab44e04a70
Landry Benimana <54643392+Landry21@users.noreply.github.com>
2022-08-20 14:38:33 -0600

    Update LeetCode_15.java

--- LeetCode_15.java
+++ LeetCode_15.java
@@ -5,18 +5,26 @@
 */
 
 public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
+   //result 
+  Set<List<Integer>> res = new HashSet<>();
+  //set that trigers all each iteration
+  Set<Integer> dups = new HashSet<>();
+ //where we'll store all the elements we've visited to avoid visiting them twice
+  Map<Integer, Integer> seen = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i)
+         //if we're adding a value to dups
             if (dups.add(nums[i])) {
+             // check if it has a compliment that adds up to a value that already in the seen HashMap
                 for (int j = i + 1; j < nums.length; ++j) {
                     int complement = -nums[i] - nums[j];
+                   //if the seen has the compliment create a temp list add the nums[j],nums[j], and the compliment 
                     if (seen.containsKey(complement) && seen.get(complement) == i) {
                         List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                     //sort the temp list and add it to our final result set 
                         Collections.sort(triplet);
                         res.add(triplet);
                     }
+                 //if the compliment is not in seen add the nums[j], i to the collections so we don't have to visit it again 
                     seen.put(nums[j], i);
                 }
             }
